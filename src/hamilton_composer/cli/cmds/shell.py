50bc0c7b1a4ac3398297e4c65f40ceec35fc70be
Charles Swartz <charles.w.swartz.vi@gmail.com>
2025-09-14 21:34:04 -0400

    Update `shell` docstrings ... again

--- src/hamilton_composer/cli/cmds/shell.py
+++ src/hamilton_composer/cli/cmds/shell.py
@@ -13,14 +13,7 @@ import click
 @click.argument("params", nargs=-1, type=str, default=None)
 @click.pass_obj
 def launch_ipython_shell(context: AppContext, params: tuple[str, ...]) -> None:
-    """
-    Launch a custom Hamilton Composer IPython shell subjected to the provided OmegaConf PARAMS.
-
-    The shell is intended for exploration or debug and will be pre-populated with the following:
-    - config: A dictionary contating the complete OmegaConf-based configuration (subject to global
-              config options and OmegaConf PARAMS).
-    - pipeline
-    """
+    """Launch a custom Hamilton Composer IPython shell subjected to the OmegaConf PARAMS."""
     from hamilton_composer.exts.ipython import launch_shell
 
     config = context.load_config(params)
